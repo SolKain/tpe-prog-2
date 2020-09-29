03ef8c772ab6d9b8fa7fdbbbbdef0d9236f392b8
Sol Kain <kain.sol01@gmail.com>
2020-09-29 20:01:17 -0300

    se agrego informacion a juego y mazo

--- src/tpe/Juego.java
+++ src/tpe/Juego.java
@@ -1,5 +1,36 @@
 package tpe;
 
+import java.util.ArrayList;
+
 public class Juego {
+	Mazo mazo;
+	Jugador jugador1;
+	Jugador jugador2;
+	
+	
+	public Mazo(Mazo mazo, Jugador jugador1, Jugador jugador2) {
+		super();
+		this.mazo = mazo;
+		this.jugador1 = jugador1;
+		this.jugador2 = jugador2;
+	}
+	public Mazo getMazo() {
+		return mazo;
+	}
+	public void setMazo(Mazo mazo) {
+		this.mazo = mazo;
+	}
+	public Jugador getJugador1() {
+		return jugador1;
+	}
+	public void setJugador1(Jugador jugador1) {
+		this.jugador1 = jugador1;
+	}
+	public Jugador getJugador2() {
+		return jugador2;
+	}
+	public void setJugador2(Jugador jugador2) {
+		this.jugador2 = jugador2;
+	
 
 }
--- src/tpe/Mazo.java
+++ src/tpe/Mazo.java
@@ -1,5 +1,16 @@
 package tpe;
 
+import java.util.ArrayList;
+
 public class Mazo {
+	ArrayList<Carta> cartas;
+
+	public Mazo() {
+		cartas = new ArrayList<>();
+	}
+	
+
+	
+	
 
 }
