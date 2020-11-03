0e8166ab1a502843b9fda5fe519ceeb04871402f
Agustina Bedrossian <agusbedro@gmail.com>
2020-11-03 13:01:44 -0800

    agregue mazo nuevo y corregi nombre y valor de las pocimas

--- src/tpe/Principal.java
+++ src/tpe/Principal.java
@@ -5,30 +5,36 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Mazo mazo = new Mazo();
-		mazo.addPocima(new PocimaValorFijo("Valor fijo", 200));
-		mazo.addPocima(new PocimaValorFijo("Valor fijo", 150));
-		mazo.addPocima(new PocimaValorFijo("Valor fijo", 1000));
-		mazo.addPocima(new PocimaValorFijo("Valor fijo", 340));
-		mazo.addPocima(new PocimaIncremento("Pocima incremento", 1.5));
-		mazo.addPocima(new PocimaIncremento("Pocima incremento", 1.7));
-		mazo.addPocima(new PocimaIncremento("Pocima decremento", 0.3));
-		mazo.addPocima(new PocimaIncremento("Pocima incremento", 1.1));
-		mazo.addPocima(new PocimaSelectiva("Pocima selectiva", "fuerza", 1.5));
-		mazo.addPocima(new PocimaSelectiva("Pocima selectiva", "fuerza", 0.7));
-		mazo.addPocima(new PocimaSelectiva("Pocima selectiva", "peso", 1.2));
-		mazo.addPocima(new PocimaSelectiva("Pocima selectiva", "peso", 0.2));
+		mazo.addPocima(new PocimaValorFijo("Pocima quiero vale cuatro", 4));
+		mazo.addPocima(new PocimaValorFijo("Pocima quiero vale cuatro", 4));
+		mazo.addPocima(new PocimaValorFijo("Pocima numero magico", 23));
+		mazo.addPocima(new PocimaValorFijo("Pocima numero magico", 23));
+		mazo.addPocima(new PocimaIncremento("Pocima kriptonita", 0.25));
+		mazo.addPocima(new PocimaIncremento("Pocima kriptonita", 0.25));
+		mazo.addPocima(new PocimaIncremento("Pocima reductor de plomo", 0.55));
+		mazo.addPocima(new PocimaIncremento("Pocima reductor de plomo", 0.55));
+		mazo.addPocima(new PocimaIncremento("Pocima fortalecedora", 1.2));
+		mazo.addPocima(new PocimaIncremento("Pocima fortalecedora", 1.2));
+		mazo.addPocima(new PocimaIncremento("Pocima fortalecedora plus", 1.5));
+		mazo.addPocima(new PocimaIncremento("Pocima fortalecedora plus", 1.5));
+		mazo.addPocima(new PocimaSelectiva("Pocima selectiva", "fuerza", 1.35));
+		mazo.addPocima(new PocimaSelectiva("Pocima selectiva", "fuerza", 1.35));
+		mazo.addPocima(new PocimaSelectiva("Pocima selectiva", "peso", 1.43));
+		mazo.addPocima(new PocimaSelectiva("Pocima selectiva", "peso", 1.43));
 	
 		Cocktail cocktail1 = new Cocktail("Cocktail");
 		Cocktail cocktail2 = new Cocktail("Cocktail");		
-		cocktail1.addPocima(new PocimaValorFijo("Valor fijo", 500));
-		cocktail1.addPocima(new PocimaIncremento("Pocima incremento", 1.2));	
-		cocktail2.addPocima(new PocimaSelectiva("Pocima selectiva", "fuerza", 1.1));
-		cocktail2.addPocima(new PocimaIncremento("Pocima decremento", 0.7));
+		cocktail1.addPocima(new PocimaValorFijo("Pocima numero magico", 23));
+		cocktail1.addPocima(new PocimaIncremento("Pocima reductor de plomo", 0.55));	
+		cocktail2.addPocima(new PocimaSelectiva("Pocima selectiva", "fuerza", 1.35));
+		cocktail2.addPocima(new PocimaIncremento("Pocima fortalecedora", 1.2));
 		
 		mazo.addPocima(cocktail2);
 		mazo.addPocima(cocktail1);
 		
-		VisorMazo.cargarMazo("superheroes.json", mazo);
+		//VisorMazo.cargarMazo("superheroes.json", mazo);
+		VisorMazo.cargarMazo("autos.json", mazo);
+		
 		
 		Timbero timb1 = new Timbero();
 		Timbero timb2 = new Timbero();
